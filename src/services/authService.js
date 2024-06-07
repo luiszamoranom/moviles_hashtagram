028e14b66cde29b0dafa32a27012faeab3071533
Bastián Núñez <bastinuez@hotmail.com>
2024-06-07 12:24:46 -0400

    fix(rutas): se ajustan archivos y rutas

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,26 @@
 const BASE_URL = 'http://localhost:3000/api/auth'
+import axios from "axios"
+
+const API_URL = import.meta.env.VITE_API_ENDPOINT;
 
 export const login = (data) => {
   console.log("Iniciando sesión en la API mi chamo", BASE_URL)
   console.log("Información de inicio de sesión", data)
+}
+export const registrar = async (correo, nombre_completo, nombre_usuario,contrasena) => {
+  try {
+    const response = await axios.post(`${API_URL}/usuario/registrar`, {
+      nombre_completo,nombre_usuario,email:correo,contrasena});
+    console.log(response)
+    if (response.status === 201) {
+      //console.log("Usuario registrado correctamente.");
+      return true;
+    }
+    else {
+      return false;
+    }
+  } catch (error) {
+    //console.error('Error al registrar usuario', error);
+    return false;
+  }
 }
