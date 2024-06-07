5e1fb068e1a78fb6812c6de0968bf6830a2c9845
Bastián Núñez <bastinuez@hotmail.com>
2024-06-07 13:26:22 -0400

    FEAT(conexion): se fixea la conexion

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 const BASE_URL = 'http://localhost:3000/api/auth'
 import axios from "axios"
 
-const API_URL = import.meta.env.VITE_API_ENDPOINT;
+const API_URL = import.meta.env.REACT_APP_BACKEND_URL;
 
 export const login = (data) => {
   console.log("Iniciando sesión en la API mi chamo", BASE_URL)
@@ -9,7 +9,7 @@ export const login = (data) => {
 }
 export const registrar = async (correo, nombre_completo, nombre_usuario,contrasena) => {
   try {
-    const response = await axios.post(`${API_URL}/usuario/registrar`, {
+    const response = await axios.post(`http://34.176.83.207:9999/usuario/registrar`, {
       nombre_completo,nombre_usuario,email:correo,contrasena});
     console.log(response)
     if (response.status === 201) {
