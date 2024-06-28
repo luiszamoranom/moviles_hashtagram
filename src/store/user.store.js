dd46399875a656b6a25fb4823e119146c2cb6fc4
AlonsoxNa <aorostica20@alumnos.utalca.cl>
2024-06-28 10:57:48 -0400

    feat(storage): se añade persistencia de los datos con zustand y capacitor/preferences

---
+++ src/store/user.store.js
@@ -0,0 +1,29 @@
+import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
+import { customCapacitorStorage } from './storages/capacitor.storage';
+
+
+export const useUserStore = create(
+  persist((set) => ({
+    
+
+    user: {
+      username: '',
+      token: ''
+    },
+
+    handleLogin: (username, token) => set({ 
+      user: {
+        username, 
+        token 
+      }
+    }),
+    handleLogout: () => {
+      set({ token: '' })
+      
+    },
+  }), {
+    name: 'user-storage',
+    storage: customCapacitorStorage
+  })
+)
