d51722181e4ea3e5b8e74729542075fc2bf1e3fb
Bastián Núñez <bastinuez@hotmail.com>
2024-07-03 11:51:03 -0400

    feat(login): si el usuario inicio sesion ingresa directamente a la app

--- src/routers/PublicRoutes.jsx
+++ src/routers/PublicRoutes.jsx
@@ -1,16 +1,19 @@
 import { Navigate } from 'react-router-dom';
 import { useUserStore } from '../store/user.store';
 import { jwtDecode } from 'jwt-decode';
+import usuarioStore from '../store/usuarioStore';
+import useUsuarioCache from '../hooks/usuario/useUsuarioCache';
 
 export const PublicRoutes = ({children}) => {
   
-  const user = useUserStore(state => state.user)
+  //const user = useUserStore(state => state.user)
+  const {userCredentials} =useUsuarioCache()
   
-  if (!user.token) {
+  if (!userCredentials?.accessToken) {
     return <>{children}</>
   }
 
-  const tokenString = JSON.stringify(user.token);
+  const tokenString = JSON.stringify(userCredentials?.accessToken);
   
   try {
     const decodedToken = jwtDecode(tokenString);
