a752b0c66321d83e1cd300d42378fe9c4c0dddc2
Luis Zamorano <lzamorano20@alumnos.utalca.cl>
2024-07-05 07:42:24 +0200

    feat(me-gusta): dando y quitando me-gusta funcional en feed

--- src/services/meGustaService.js
+++ src/services/meGustaService.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const API_URL = import.meta.env.VITE_REACT_APP_BACKEND_URL;
 
-export const registrarMeGusta = async ({in_interactuadorId, in_fotoId}) => {
+export const registrarMeGusta = async (in_interactuadorId, in_fotoId) => {
   try {
     const response = await axios.post(`${API_URL}/me-gusta/registrar`, {
       interactuadorId: in_interactuadorId,
@@ -19,9 +19,23 @@ export const registrarMeGusta = async ({in_interactuadorId, in_fotoId}) => {
   }
 }
 
-export const eliminarMeGusta = async ({in_interactuadorId, in_fotoId}) => {
+export const eliminarMeGusta = async (in_interactuadorId, in_fotoId) => {
     try {
-      const response = await axios.delete(`${API_URL}/me-gusta/eliminar`, {
+      const response = await axios.delete(`${API_URL}/me-gusta/eliminar?interactuadorId=${in_interactuadorId}&fotoId=${in_fotoId}`)
+      if ( response.status === 200 ) {
+        return {success: true, message: response.data};
+      } else {
+        return {success: false, status: response.status};
+      }
+  
+    } catch (error) {
+      return {success: false, status: error.response.status};
+    }
+  }
+
+  export const saberSiUsuarioDioMeGustaAFoto = async (in_interactuadorId, in_fotoId) => {
+    try {
+      const response = await axios.get(`${API_URL}/me-gusta/saber-si-usuario-dio-like-a-una-foto`, {
         interactuadorId: in_interactuadorId,
         fotoId: in_fotoId
       })
