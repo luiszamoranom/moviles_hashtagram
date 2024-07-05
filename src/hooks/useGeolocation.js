bf7b7860f68baad06f2969975f1dc57ad247c3cd
Bastián Núñez <bastinuez@hotmail.com>
2024-07-04 23:03:46 -0400

    fix(GPS): exige el gps para capturar ubicacion

--- src/hooks/useGeolocation.js
+++ src/hooks/useGeolocation.js
@@ -1,13 +1,39 @@
 import { Geolocation } from '@capacitor/geolocation';
+import { useState, useEffect } from 'react';
+import { Capacitor } from '@capacitor/core';
 
 const useGeolocation = () => {
+    const [position, setPosition] = useState(null);
+    const [error, setError] = useState(null);
+
     const getPosition = async () => {
-        const coordinates = await Geolocation.getCurrentPosition();
-        return coordinates;
+        try {
+            const permission = await Geolocation.requestPermissions();
+            if (permission.location === 'granted') {
+                const coordinates = await Geolocation.getCurrentPosition();
+                setPosition(coordinates);
+                return coordinates;
+            } else {
+                throw new Error('Geolocation permission not granted');
+            }
+        } catch (err) {
+            setError(err.message);
+            console.error('Error getting geolocation', err);
+            return null;
+        }
     };
+
+    useEffect(() => {
+        if (Capacitor.isNative) {
+            getPosition();
+        }
+    }, []);
+
     return {
+        position,
+        error,
         getPosition
     }
 }
 
-export default useGeolocation
+export default useGeolocation;
